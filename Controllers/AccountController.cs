c967f9256ec0e39eaa0f6a1d46ac11c8042c4a0c
maryshinyg <maryshiny16@gmail.com>
2026-05-10 14:30:18 +0530

    Added Login and register pages

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,66 @@
+using EchoBlog.Models.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Models.ViewModels;
+
+namespace EchoBlog.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            var identityUser = new IdentityUser
+            {
+                UserName = model.Username,
+                Email = model.Email
+            };
+            var identityResult = await _userManager.CreateAsync(identityUser, model.Password);
+            if (identityResult.Succeeded)
+            {
+                //assign this user to "user" role
+                var roleIdentityResult = await _userManager.AddToRoleAsync(identityUser, "user");
+                if (roleIdentityResult.Succeeded)
+                {
+                    //show success notification
+                    return RedirectToAction("Register");
+                }
+            }
+            //show error notification
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            var signInResult = await _signInManager.PasswordSignInAsync(model.Username, model.Password, false, false);
+            if(signInResult != null && signInResult.Succeeded)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            //show error notification
+            return View();
+        }
+    }
+}
---
+++ Models/ViewModels/LoginViewModel.cs
@@ -0,0 +1,8 @@
+namespace EchoBlog.Models.ViewModels
+{
+    public class LoginViewModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
